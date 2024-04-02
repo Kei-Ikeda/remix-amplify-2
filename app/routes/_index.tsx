7a1db829eeb72e022b51737bea679ea560f16d03
Kei-Ikeda <g9it56@gmail.com>
2024-04-02 21:36:21 +0900

    add pokemon with loader

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,7 @@
 import type { MetaFunction } from "@remix-run/node";
+import { json } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { PokeAPI } from "pokeapi-types";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,35 +10,29 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const loader = async () => {
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon/pikachu");
+
+  const responseJson:PokeAPI.Pokemon = await response.json();
+
+  // Return the data as JSON
+  return json({ responseJson });
+};
+
 export default function Index() {
+  const { responseJson } = useLoaderData<typeof loader>();
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <h1>Welcome to Remix with Poke</h1>
+      <div>
+        <p>{responseJson.name}</p>
+        <img
+          src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
+          alt="pika"
+        />
+      </div>
+      {JSON.stringify(responseJson)}
     </div>
   );
 }
